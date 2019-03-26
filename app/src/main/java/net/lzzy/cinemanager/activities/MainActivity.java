cd254414838d2c545930333eb6d26bbf9b1e7641
Administrator <1491807832@qq.com>
2019-03-26 18:02:04 +0800

    添加订单影院列表3月26号第10节课

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package net.lzzy.cinemanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +9,14 @@ import android.widget.SearchView;
 import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
+import net.lzzy.cinemanager.fragments.CinemasFragment;
+import net.lzzy.cinemanager.fragments.OrdersFragment;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private FragmentManager manager = getSupportFragmentManager();
     private View layoutMenu;
     private TextView tvTitle;
     private SearchView search;
@@ -51,6 +55,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        layoutMenu.setVisibility(View.GONE);
+        switch (v.getId()) {
+            case R.id.bar_order:
+                break;
+            case R.id.bar_add_cinema:
+                tvTitle.setText("影院列表");
+                manager.beginTransaction()
+                        .replace(R.id.fragment_container, new CinemasFragment())
+                        .commit();
+                break;
+            case R.id.bar_add_order:
+                tvTitle.setText("我的订单");
+                manager.beginTransaction()
+                        .replace(R.id.fragment_container, new OrdersFragment())
+                        .commit();
+                break;
+            default:
+                break;
+        }
     }
 }
+
